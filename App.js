e909c2bb97320149a301b659c81672563381c96d
Anirudh Iyer <a24iyer@uwaterloo.ca>
2020-12-22 12:29:41 +0530

    added button

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { AppLoading, Permissions } from 'expo';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { Audio } from 'expo-av';
+
+
 
 export default function App() {
+  const [recording, setRecording] = useState();
+
+  const onPress = () => startRecording()
+  const onRelease = () => stopRecording()
+
+  async function startRecording() {
+    try {
+      console.log('Requesting permissions..');
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+      console.log('Starting recording..');
+      const recording = new Audio.Recording();
+      await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+      await recording.startAsync();
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    const uri = recording.getURI();
+    console.log('Recording stopped and stored at', uri);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity
+        style={styles.ButtonStyle}
+        onPress={onPress}
+        onRelease={onRelease}
+      >
+        <Image
+          source={require('./assets/round_mic_black_48pt_3x.png')}
+          style={styles.ImageIconStyle}
+        />
+      </TouchableOpacity>
+      <Text>Press to record a voice memo</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +66,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  ButtonStyle: {
+    backgroundColor: '#9DEBD9',
+    padding: 10
+  }
 });

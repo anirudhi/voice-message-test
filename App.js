49f5d8cd8c3c3bfa594b189effe20b412424413a
Anirudh Iyer <a24iyer@uwaterloo.ca>
2020-12-29 23:21:05 +0530

    added ui

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { AppLoading, Permissions } from 'expo';
-import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, Image } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  Image,
+  Dimensions,
+  TextInput
+} from 'react-native';
 import Slider from '@react-native-community/slider';
 import { Audio } from 'expo-av';
 import * as FileSystem from "expo-file-system";
+import IconFontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import IconFour from 'react-native-vector-icons/Ionicons';
 // import WaveForm from 'react-native-audiowaveform';
 
 const BACKGROUND_COLOR = "#FFF8ED";
+const { width, height } = Dimensions.get('window');
+const baseColor = '#1C5870';
 
 
 function WaveformList(props) {
@@ -24,10 +38,28 @@ function WaveformList(props) {
   return (<Text>{props.recordingArr[props.recordingArr.length - 1]}</Text>)
 }
 
+function RecordingMessage(props) {
+
+  <View style={{ backgroundColor: 'transparent', padding: 10, borderRadius: 10, width: width * 0.8 }}>
+    <View style={{ backgroundColor: chatColor, borderRadius: 10 }}>
+      <Text
+        style={{ fontSize: width * 0.05, color: 'black', paddingVertical: height * 0.01, paddingHorizontal: width * 0.04 }}
+      >
+        {item.body}
+      </Text>
+      <Text
+        style={{ fontSize: width * 0.03, color: 'black', marginBottom: height * 0.01, textAlign: 'right', alignSelf: 'stretch', marginRight: width * 0.04 }}
+      >
+        {moment(Date.now()).format('lll').toString()}
+      </Text>
+    </View>
+  </View>
+}
+
 export default function App() {
   const [recording, setRecording] = useState(null);
+  let [message, setMessage] = useState('');
   const [recordingArr, setRecordingArr] = useState([]);
-  const [hasRecordingPermission, setRecordingPermission] = useState(false);
   const [recordingStatus, setRecordingStatus] = useState({
     isRecording: false,
     recordingDuration: null,
@@ -43,6 +75,100 @@ export default function App() {
   let isSeeking = false
 
   recordingSettings = Audio.RECORDING_OPTIONS_PRESET_LOW_QUALITY;
+
+  const renderRecordBox = () => {
+    return (
+      <View
+        style={{
+          width: width,
+          backgroundColor: '#e8eaed',
+          height: height * 0.11,
+          paddingHorizontal: width * 0.02,
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          alignItems: 'center'
+        }}
+      >
+        <View
+          style={{ height: height * 0.064, borderColor: 'gray', borderWidth: 1, width: width * 0.82, borderRadius: 10, backgroundColor: 'white', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 10 }}
+        >
+          <TouchableOpacity
+            style={{ alignItems: 'center', justifyContent: 'center' }}
+            onPress={() => stopRecording()}
+          >
+            <IconFour
+              name="send"
+              color="green"
+              size={width * 0.06}
+            />
+          </TouchableOpacity>
+          <Text>{recordingStatus.recordingDuration}</Text>
+          <TouchableOpacity
+            style={{ alignItems: 'center', justifyContent: 'center' }}
+            onPress={() => stopRecording()}
+          >
+            <IconFour
+              name="close"
+              color="red"
+              size={width * 0.06}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+
+  const renderSendBox = () => {
+    return (
+      <View
+        style={{
+          width: width,
+          backgroundColor: '#e8eaed',
+          height: height * 0.11,
+          paddingHorizontal: width * 0.02,
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          alignItems: 'center'
+        }}
+      >
+        <View
+          style={{ height: height * 0.064, borderColor: 'gray', borderWidth: 1, width: width * 0.82, borderRadius: 10, backgroundColor: 'white', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 10 }}
+        >
+          <TextInput
+            style={{ height: height * 0.05, width: width * 0.68, backgroundColor: 'white', fontSize: width * 0.04 }}
+            placeholder={'input'}
+            multiline={true}
+            numberOfLines={40}
+            value={message}
+            onChangeText={messageText => {
+              setMessage(messageText)
+            }}
+          />
+          <TouchableOpacity
+            style={{ alignItems: 'center', justifyContent: 'center' }}
+            onPress={() => startRecording()}
+          >
+            <IconFour
+              name="mic"
+              color="gray"
+              size={width * 0.06}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity
+          style={{ alignItems: 'center', justifyContent: 'center', backgroundColor: baseColor, height: width * 0.12, width: width * 0.12, borderRadius: width * 0.12 }}
+          onPress={() => sendMessage()}
+        >
+          <Icon
+            name="send"
+            color="white"
+            size={width * 0.06}
+          />
+        </TouchableOpacity>
+      </View>
+    )
+  }
 
   function updateScreenForRecordingStatus(status) {
     if (status.canRecord) {
@@ -143,6 +269,7 @@ export default function App() {
   };
 
   async function startRecording() {
+    setRecording(null)
     try {
       console.log('Requesting permissions..');
       await Audio.requestPermissionsAsync();
@@ -151,11 +278,11 @@ export default function App() {
         playsInSilentModeIOS: true,
       });
       console.log('Starting recording..');
-      const recording = new Audio.Recording();
-      await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
-      status = await recording.startAsync();
-      updateScreenForRecordingStatus(status)
-      setRecording(recording);
+      const new_recording = new Audio.Recording();
+      await new_recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+      new_recording.setOnRecordingStatusUpdate(updateScreenForRecordingStatus);
+      status = await new_recording.startAsync();
+      setRecording(new_recording);
       console.log('Recording started');
     } catch (err) {
       console.error('Failed to start recording', err);
@@ -164,11 +291,11 @@ export default function App() {
 
   async function stopRecording() {
     console.log('Stopping recording..');
-    setRecording(null);
     status = await recording.stopAndUnloadAsync();
     updateScreenForRecordingStatus(status)
     const uri = recording.getURI();
     setRecordingArr(recordingArr.concat(uri))
+    setRecording(null);
     console.log('Recording stopped and stored at', uri);
     // Get sound for playback
     const { new_sound, status } = await recording.createNewLoadedSoundAsync(
@@ -184,6 +311,9 @@ export default function App() {
     sound = new_sound;
   }
 
+  const renderBottomBox = (
+    recordingStatus.isRecording ? renderRecordBox() : renderSendBox()
+  )
 
   let waveform = recordingArr.length > 0 ?
     recordingArr.map((info) => {
@@ -224,16 +354,16 @@ export default function App() {
         </TouchableHighlight>
         <Slider
           style={styles.playbackSlider}
-          // trackImage={Icons.TRACK_1.module}
-          // thumbImage={Icons.THUMB_1.module}
           value={getSeekSliderPosition}
           onValueChange={onSeekSliderValueChange}
           onSlidingComplete={onSeekSliderSlidingComplete}
-        // disabled={!this.state.isPlaybackAllowed || this.state.isLoading}
         />
         <Text>
           {getPlaybackTimestamp()}
         </Text>
+      </View>
+      <View>
+        {renderBottomBox}
       </View>
     </View>
   );
